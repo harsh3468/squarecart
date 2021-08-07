87d7a9751d783891741cb294e7d29e2ef24db35d
harsh3468 <harshkumargoel.3468@gmail.com>
2021-08-07 09:15:37 +0530

    first commit

--- src/component/OrderPlaced.js
+++ src/component/OrderPlaced.js
@@ -6,7 +6,7 @@ export class OrderPlaced extends Component {
             <div className="order-placed-section">
                 <div className="order-placed-message">
                     <h3>Order Placed</h3>
-                    <img src="/img/order-placed-icon.jpg"></img>
+                    <img src="./img/order-placed-icon.jpg"></img>
                     <p>Order id : {this.props.match.params.id}</p>
                     <p>Thanku for shopping from squarecart</p>
                     <div className="order-placed-button">
